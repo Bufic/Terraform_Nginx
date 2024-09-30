901cc3992956ed4df568a9dba677fba0c93fe739
bufic <fubaraemmanueljnr@gmail.com>
2024-09-30 18:25:10 +0100

    output.tf file edited

--- output.tf
+++ output.tf
@@ -2,6 +2,6 @@ output "ec2_nginx_ip" {
   value = aws_instance.group10_nginx_server.public_ip
 }
 
-output "ec2_jenkins_ip" {
-  value = aws_instance.group10_jenkins_server.public_ip
-}
+# output "ec2_jenkins_ip" {
+#   value = aws_instance.group10_jenkins_server.public_ip
+# }
